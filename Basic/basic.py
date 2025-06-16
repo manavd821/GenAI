1215c142fcfe783dd27907a17a2c8404234cb63a
manavok <mitmanav6805@gmail.com>
2025-06-16 22:03:05 +0530

    Refactor chat functionality to handle user input and maintain conversation history

--- Basic/basic.py
+++ Basic/basic.py
@@ -1,12 +1,53 @@
 from google import genai
 from dotenv import load_dotenv
 import os
+import asyncio
 load_dotenv()
 client = genai.Client(api_key=os.getenv('API_KEY'))
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents="Explain how AI works in a few words",
-)
+# response = client.models.generate_content(
+#     model="gemini-2.0-flash",
+#     contents=[
+#         {
+#             'role' : 'user',
+#             'parts' : [{'text' : 'My name is Manav'}]
+#         },
+#         {
+#             'role' : 'model',
+#             'parts' : [{'text' : 'Hare Krsna'}]
+#         },
+#         {
+#             'role' : 'user',
+#             'parts' : [{'text' : 'What is my name?'}]
+#         }
+#     ]
+# )
+# print(response.text)
 
-print(response.text)
+History = []
+async def chatting(userProblem):
+    History.append({
+        "role" : 'user',
+        "parts" : [{"text": userProblem }]
+    })
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents = History
+        )
+        print(response.text)
+
+    except Exception as e:
+        print(f"Error in response: {e}")
+    
+    # print(f"History : {History}\n")
+
+    
+async def main():
+    userProblem = str(input('Hare Krsna, Ask me anything:'))
+    await chatting(userProblem)
+    await main()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())  
