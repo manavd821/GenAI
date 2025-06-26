9b52486adf6bb93e41562a5d6364255b59cb98e0
manavok <mitmanav6805@gmail.com>
2025-06-26 10:04:51 +0530

    Add string output parser for generating detailed reports and summaries

---
+++ LangChain/Output/Unstructured/string_ouput_parser.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model = 'gemini-2.0-flash')
+temp1 = PromptTemplate(
+    template='Write detail report on {topic}',
+    input_variables=['topic'],
+    validate_template=True
+)
+temp2 = PromptTemplate(
+    template='Write 5 line summary on following text.\n {text}',
+    input_variables=['text'],
+    validate_template=True
+)
+prompt1 = temp1.invoke({'topic' : 'black hole'})
+result1 = llm.invoke(prompt1)
+print('result 1:\n', result1)
+
+prompt2 = temp2.invoke({'text' : result1.content})
+result2 = llm.invoke(prompt2)
+print('\n\n\n',result2.content)
